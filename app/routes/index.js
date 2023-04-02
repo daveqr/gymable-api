33e32619264fd59d4d4f25d4b27159bacf5718a6
Dave Daniels <dave.daniels@daveramsey.com>
2023-04-02 17:29:12 -0500

    Add local auth for users

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const localStrategy = require('../strategies/local');
+
+passport.use(localStrategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  // In a real application, you would retrieve the user's data from a database or other source using the ID
+  const user = { id: 1, username: 'myuser' };
+  done(null, user);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- app/routes/users.js
+++ app/routes/users.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
 
+const passport = require('passport');
+const localStrategy = require('../strategies/local');
+
+passport.use(localStrategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  // In a real application, you would retrieve the user's data from a database or other source using the ID
+  const user = { id: 1, username: 'myuser' };
+  done(null, user);
+});
+
 /* GET users listing. */
-router.get('/', async (req, res) => {
+router.get('/', passport.authenticate('local', { session: false }), async (req, res) => {
   try {
     const users = await User.findAll();
     res.json(users);
@@ -12,5 +27,15 @@ router.get('/', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// router.get('/', async (req, res) => {
+//   try {
+//     const users = await User.findAll();
+//     res.json(users);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Server Error');
+//   }
+// });
 
 module.exports = router;
